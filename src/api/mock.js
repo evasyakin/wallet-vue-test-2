153bcd8c8304b51fb56076a48a99fb088b00f241
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-10-10 00:34:16 +0300

    mock: Вынес дефолтные данные и добавил перенос блоков

--- src/api/mock.js
+++ src/api/mock.js
@@ -2,6 +2,11 @@
  * Мокнутые данные для имитации запросов по API.
  */
 
+const defaults = {
+    sizeX: 300, sizeY: 100,
+    between: 10,
+}
+
 var blocks = []
 
 for (let i = 0; i < 5; i++) {
@@ -9,8 +14,10 @@ for (let i = 0; i < 5; i++) {
         id: i + 1,
         title: `Title ${i+1}`,
         content: 'Random content',
-        posX: i * 300, posY: 0, posZ: 0,
-        sizeX: 300, sizeY: 100,
+        posX: i % 3 * (defaults.sizeX + defaults.between) + defaults.between,
+        posY: Math.floor(i / 3) * (defaults.sizeY + defaults.between) + defaults.between, 
+        posZ: 0,
+        sizeX: defaults.sizeX, sizeY: defaults.sizeY,
         intersecting: i < 1 ? false : true, // пересекаемый (может пересекать другие блоки)
     })
 }
