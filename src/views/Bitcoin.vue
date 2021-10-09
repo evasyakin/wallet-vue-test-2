d74dc815c026ca13017543e4ab09582e75c20c09
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-10-09 19:10:12 +0300

    views: Добавил страницы Desktop и Bitcoin

---
+++ src/views/Bitcoin.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bitcoin-history">
+        <h1 class="page-title">Bitcoin history</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'History',
+};
+</script>
