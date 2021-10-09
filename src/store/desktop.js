944c7d6b7318738c45efc1ed7c5a93937f453042
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-10-09 20:44:25 +0300

    store: Добавил обновление блока в сторе desktop,js

--- src/store/desktop.js
+++ src/store/desktop.js
@@ -12,11 +12,20 @@ const actions = {
     load: ({ commit }) => {
         api.desktop.load(blocks => { commit('setBlocks', blocks) })
     },
+    updateBlock: ({ commit }, block) => {
+        // api.desktop.updateBlock(block) // если нужно отправить на бэк
+        commit('updateBlock', block)
+    },
 }
 
 const mutations = {
     setBlocks: (state, blocks) => {
         state.blocks = blocks
+    },
+    updateBlock: (state, block) => {
+      let index = state.blocks.findIndex(item => item.id == block.id)
+      if (index === -1) console.error(`Block with id ${block.id} not found`)
+      else state.blocks[index] = block
     },
 }
 
