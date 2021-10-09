37e4fa1d387535d6c651fb9cb66e8c8bbb6eb85e
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-10-09 20:45:06 +0300

    views: Добавил ресайз блоков стола

--- src/views/parts/DesktopBlock.vue
+++ src/views/parts/DesktopBlock.vue
@@ -2,6 +2,7 @@
     <div class="block" :style="blockStyle">
         <h2 class="block-title">{{ block.title }}</h2>
         <p class="block-content">{{ block.content }}</p>
+        <div class="resize" @mousedown="resizeStart" title="Нажмите и тяните чтобы изменить размер блока"></div>
     </div>
 </template>
 
@@ -10,10 +11,65 @@ export default {
     props: { 
         block: { type: Object },
     },
+    data () {
+        return {
+            resize: false,
+            desktopOffset: {x: 0, y: 0},
+            cursorOffset: {x: 0, y: 0},
+        }
+    },
     computed: {
         blockStyle () {
             return `left: ${this.block.posX}px; top: ${this.block.posY}px; width: ${this.block.sizeX}px; height: ${this.block.sizeY}px;`
         },
+    },
+    methods: {
+        setCursorOffset(e) {
+            let [x, y] = this.getNodeFullOffset(e.target)
+            x = e.pageX - x
+            y = e.pageY - y
+            this.cursorOffset = {x, y}
+        },
+        getNodeFullOffset (node) {
+            let x = 0, y = 0
+            while (node) {
+                x += node.offsetLeft
+                y += node.offsetTop
+                node = node.offsetParent
+            }
+            return [x, y]
+        },
+
+        resizeStart (e) {
+            e.stopPropagation()
+            this.resize = true
+            this.setCursorOffset(e)
+        },
+        resizeProcess (e) {
+            if (this.resize) {
+                this.block.sizeX = e.pageX - this.block.posX - this.desktopOffset.x + this.cursorOffset.x
+                this.block.sizeY = e.pageY - this.block.posY - this.desktopOffset.y + this.cursorOffset.y
+            }
+        },
+        resizeEnd () {
+            if (this.resize) {
+                this.resize = false
+                this.updateBlock()
+            }
+        },
+
+        updateBlock () {
+            this.$store.dispatch('desktop/updateBlock', this.block)
+        },
+    },
+    created () {
+        window.addEventListener('mousemove', this.resizeProcess)
+        window.addEventListener('mouseup', this.resizeEnd)
+    },
+    mounted () {
+        let desktopNode = document.querySelector('#app .desktop')
+        this.desktopOffset.y = desktopNode.offsetTop
+        this.desktopOffset.x = desktopNode.offsetLeft
     },
 };
 </script>
@@ -27,4 +83,17 @@ export default {
 }
 .block-title {border-bottom: solid #ddd 1px; font-size: 16px; margin-bottom: 10px; padding-bottom: 10px;}
 .block-content {font-size: 15px;}
+.resize {
+    height: 20px; width: 20px; padding-top: 10px; overflow: hidden;
+    clip-path: polygon(100% 0%, 100% 100%, 0% 100%); cursor: nw-resize;
+    position: absolute; bottom: 1px; right: 1px;
+}
+.resize::before, .resize::after {
+    display: block; content: '';
+    border-bottom: solid 1px #333; border-top: solid 1px #333;
+    height: 3px; width: 30px;
+    transform: rotate(-45deg);
+}
+.resize::before {}
+.resize::after {margin-top: 3px;}
 </style>
