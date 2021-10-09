da58364053a730c0104f041a3c16d623682959ff
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-10-09 21:54:19 +0300

    mock: Добавил для блоков позицию по оси Z

--- src/api/mock.js
+++ src/api/mock.js
@@ -9,7 +9,7 @@ for (let i = 0; i < 5; i++) {
     id: i + 1,
     title: `Title ${i+1}`,
     content: 'Random content',
-    posX: i * 300, posY: 0,
+    posX: i * 300, posY: 0, posZ: 0,
     sizeX: 300, sizeY: 100,
   })
 }
