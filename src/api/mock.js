876f351b380044808c0d5a4a822dadccbd9bffe9
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-10-09 19:18:25 +0300

    api: Добавил прослойку для api и мокнутые данные для десктопа

---
+++ src/api/mock.js
@@ -0,0 +1,21 @@
+/**
+ * Мокнутые данные для имитации запросов по API.
+ */
+
+var blocks = []
+
+for (let i = 0; i < 5; i++) {
+  blocks.push({
+    id: i + 1,
+    title: `Title ${i+1}`,
+    content: 'Random content',
+    posX: i * 300, posY: 0,
+    sizeX: 300, sizeY: 100,
+  })
+}
+
+const desktop = { blocks }
+
+export default {
+    desktop
+};
