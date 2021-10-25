c6be47b2d0c14d4a2cf8e3302e17f108eabf82c8
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-10-25 14:14:58 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
 import router from './router'
 
 Vue.config.productionTip = false
+
+// добавление своего стора
+import Plugin from './core/Plugin'
+import store from './store/store'
+Vue.use(new Plugin('store', store))
 
 new Vue({
   store,
